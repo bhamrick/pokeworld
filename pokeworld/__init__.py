bc36983920877f5c3720b24c21e2dbeee149d387
Brian Hamrick <brian.c.hamrick@gmail.com>
2014-02-05 11:58:33 -0800

    Handle sigterm gracefully

--- pokeworld/__init__.py
+++ pokeworld/__init__.py
@@ -2,10 +2,17 @@ from pyramid.config import Configurator
 
 from .config import routes
 
+import signal
+import sys
+
+def sigterm_handler(signum, frame):
+    sys.exit(0)
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
     config.include(routes)
+    signal.signal(signal.SIGTERM, sigterm_handler)
     return config.make_wsgi_app()
 
