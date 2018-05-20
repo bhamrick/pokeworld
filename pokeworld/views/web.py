6b4a5da00969e2f5fe2f2ff8e546c12cd9a77003
Brian Hamrick <brian.c.hamrick@gmail.com>
2018-05-20 15:59:25 -0700

    Redirect to new site

--- pokeworld/views/web.py
+++ pokeworld/views/web.py
@@ -1,27 +1,40 @@
 from pokeworld.lib.session import web, ajax, data
 from pokeworld.constants import PATH_PREFIX
 
+from pyramid.httpexceptions import HTTPMovedPermanently
+
 import os.path
 import simplejson as json
 
 class WebView:
-    @web(template="web/home.tmpl")
+    @staticmethod
     def home(request):
-        return {'project' : 'pokeworld'}
+        return HTTPMovedPermanently(location='https://www.extratricky.com/pokeworld')
 
-    @web(template="web/view_map.tmpl")
+    @staticmethod
     def view_map(request):
         game = request.matchdict['game']
         map_id = request.matchdict['mapid']
-        filename = os.path.join(PATH_PREFIX, 'resources/%s/maps/%s.json' % (game, map_id))
-        with open(filename) as f:
-            map_dict = json.loads(f.read())
-        for w in map_dict['warps']:
-            w['px_x'] = w['x'] * 16
-            w['px_y'] = w['y'] * 16
-        map_dict['use_minimap'] = 'minimap_img' in map_dict
-        map_dict['game'] = game
-        return map_dict
+        return HTTPMovedPermanently(location='https://www.extratricky.com/pokeworld/%s/%s' % (game, map_id))
+
+    #@web(template="web/home.tmpl")
+    #def home(request):
+    #    return {'project' : 'pokeworld'}
+
+    #@web(template="web/view_map.tmpl")
+    #def view_map(request):
+    #    game = request.matchdict['game']
+    #    map_id = request.matchdict['mapid']
+
+    #    filename = os.path.join(PATH_PREFIX, 'resources/%s/maps/%s.json' % (game, map_id))
+    #    with open(filename) as f:
+    #        map_dict = json.loads(f.read())
+    #    for w in map_dict['warps']:
+    #        w['px_x'] = w['x'] * 16
+    #        w['px_y'] = w['y'] * 16
+    #    map_dict['use_minimap'] = 'minimap_img' in map_dict
+    #    map_dict['game'] = game
+    #    return map_dict
 
     @data(content_type='image/x-icon')
     def favicon(request):
